94d950f1d763a1d2c64b7b41e4a3c9be5e07ccb6
BHISMA ADHIKARI <bhisma@BHISMAs-MBP.attlocal.net>
2021-10-14 22:42:12 -0500

    updates

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,22 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class CalculatorTest {
+    @InjectMocks
+    Calculator calculator;
+
+    @Mock
+    Adder adder;
+
+    @Test
+    public void testAdd() {
+        Mockito.when(adder.add(1,2)).thenReturn(3);
+        Assertions.assertEquals(3, calculator.add(1,2));
+    }
 }
