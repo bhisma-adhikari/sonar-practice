e6442e29cb2a19ed1baccb56d236689e0cf13250
BHISMA ADHIKARI <bhisma@BHISMAs-MBP.attlocal.net>
2021-10-14 22:27:15 -0500

    updates

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
 public class Main {
-    boolean b = false;
-    if (b)
-    {
-        System.out.println("b is true");
+    public static void main(String[] args) {
+        boolean b = false;
+        if (b)
+        {
+            System.out.println("b is true");
+        }
     }
 }
